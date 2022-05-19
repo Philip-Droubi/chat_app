3bcf33f8b2e08d3298e39fb1586504ad7d9bc003
Philip-Droubi <83915007+Philip-Droubi@users.noreply.github.com>
2022-05-20 02:05:53 +0300

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '192.168.137.145',
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http'
             ],
